c6497373705b747150eca22256831253b72194cb
黄鑫圳 <179985152+fufu-233@users.noreply.github.com>
2024-11-12 17:13:09 +0800

    Add files via upload
    更改了topo的展示效果，使用npm install 下载相关依赖，npm run serve 启动程序

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/icon.svg">
+    <NetworkTopology />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+}
+</style>
+
+<script>
+import NetworkTopology from './NetworkTopology.vue';
+
+export default {
+  name: 'App',
+  components: {
+    NetworkTopology
+  }
+};
+</script>
+
